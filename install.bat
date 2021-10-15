edbf485b47ea85208d1db1f5771cc02d12e1f4b6
Shoubhik Maiti <ShoubhikRaj@users.noreply.github.com>
2021-10-15 10:26:44 +0100

    Fix install script

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-@echo *** Compiling ChemFiles library ***
+@echo --- Compiling ChemFiles library ---
 @rem A script to install Water_order on windows
 @cd chemfiles-3fc67b1
 @mkdir build
@@ -10,16 +10,16 @@ cmake --build . --config Release --target install
 @setlocal enableextensions
 @setlocal enabledelayedexpansion
 @if defined CXX (
-@echo *** C++ compiler is %CXX% ***
+@echo --- C++ compiler is %CXX% ---
 ) else (
 @echo Setting C++ compiler to default: Visual C++ compiler cl.exe
 @set CXX=cl
 )
 @if exist "chemfiles-install\lib\chemfiles.lib" (
-@echo *** Now compiling water_order ***
-%CXX% /EHSc /Fe:Water_order /O2 /MD /fp:fast /openmp /I.\tclap-1.2.4\include /I.\chemfiles-install\include Water_order.cpp /I.\voro-71c84c8\src Water_order.cpp voro-71c84c8\src\voro++.cc /link .\chemfiles-install\lib\chemfiles.lib ws2_32.lib advapi32.lib
+@echo --- Now compiling water_order ---
+%CXX% /EHsc /Fe:Water_order /O2 /MD /fp:fast /openmp /I.\tclap-1.2.4\include /I.\chemfiles-install\include /I.\voro-71c84c8\src Water_order.cpp voro-71c84c8\src\voro++.cc /link .\chemfiles-install\lib\chemfiles.lib ws2_32.lib advapi32.lib
 ) else (
-@echo !!! ChemFiles compilation did not finish successfully !!!
-@echo !!! Please check the CMake logs !!! 
+@echo ChemFiles compilation did not finish successfully ^^!
+@echo Please check the CMake logs ^^!
 )
-endlocal
+@endlocal
